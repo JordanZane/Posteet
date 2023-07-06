3c5c33890f4b0f9042c26641120a716500e02f97
Zanetti Jordan <jzanetti1@outlook.fr>
2023-07-06 18:41:09 +0200

    Add Home page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './components/Pages/Home';
 
 function App() {
   return (
     <>
       <Header />
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/components/Pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <main>
+        <section className="about section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <h2>About</h2>
+              </div>
+              <div className="col-lg-6"></div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
