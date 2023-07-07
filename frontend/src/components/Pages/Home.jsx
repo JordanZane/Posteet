4f3c79889217e412d8538ae985dd7695d84976cf
Zanetti Jordan <jzanetti1@outlook.fr>
2023-07-07 17:38:34 +0200

    Add about section in Home Component + styles

--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import aboutImg from '../../assets/images/about-img.png';
 
 const Home = () => {
   return (
@@ -7,10 +9,16 @@ const Home = () => {
         <section className="about section">
           <div className="container">
             <div className="row">
-              <div className="col-lg-6">
+              <div className="col-lg-6 text-content">
                 <h2>About</h2>
+                <p>Welcome to <span>Posteet</span>, your web application for note creation which offers a user-friendly platform to manage and organize your digital post-it notes.</p>
+                <div className="btn-container">
+                  <Link to='/sign-up' className='btn-style'>Sign-up for free</Link>
+                </div>
               </div>
-              <div className="col-lg-6"></div>
+              <div className="col-lg-6 img-container">
+                <img src={aboutImg} alt="Posteet - about" />
+              </div>
             </div>
           </div>
         </section>
