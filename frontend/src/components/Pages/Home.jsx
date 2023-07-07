a13225cf8dd02dda55d5c867f0d125aa91634ea5
Zanetti Jordan <jzanetti1@outlook.fr>
2023-07-07 17:44:57 +0200

    Merge on main Branch : Add about section Home Component + styles

--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import aboutImg from '../../assets/images/about-img.png';
 
 const Home = () => {
@@ -11,9 +11,15 @@ const Home = () => {
             <div className="row">
               <div className="col-lg-6 text-content">
                 <h2>About</h2>
-                <p>Welcome to <span>Posteet</span>, your web application for note creation which offers a user-friendly platform to manage and organize your digital post-it notes.</p>
+                <p>
+                  Welcome to <span>Posteet</span>, your web application for note
+                  creation which offers a user-friendly platform to manage and
+                  organize your digital post-it notes.
+                </p>
                 <div className="btn-container">
-                  <Link to='/sign-up' className='btn-style'>Sign-up for free</Link>
+                  <Link to="/sign-up" className="btn-style">
+                    Sign-up for free
+                  </Link>
                 </div>
               </div>
               <div className="col-lg-6 img-container">
