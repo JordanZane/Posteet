dd3a0b685459bc5286c617c199bdc2e1a0f0214b
Zanetti Jordan <jzanetti1@outlook.fr>
2023-07-08 19:09:06 +0200

    Add 'Manage your notes'section + styles + slider React

--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import aboutImg from '../../assets/images/about-img.png';
 
 const Home = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className="home-page">
       <main>
@@ -44,6 +56,36 @@ const Home = () => {
                   tasks, Posteet empowers you to create, edit, and prioritize
                   your notes with utmost convenience.
                 </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="management section odd-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 text-content">
+                <h2>Manage your notes</h2>
+                <p>
+                  Stay on top of your to-do list by assigning importance levels
+                  to your notes, ensuring that your most critical tasks are
+                  never overlooked. Need a little nudge to keep things on track?
+                  Posteet has you covered with customizable reminders, allowing
+                  you to set timely alerts for important deadlines or
+                  appointments.
+                </p>
+              </div>
+              <div className="col-lg-6 img-container">
+                <Slider {...sliderSettings}>
+                  <div className="slide">
+                    <img src={aboutImg} alt="slide-img" />
+                  </div>
+                  <div className="slide">
+                    <img src={aboutImg} alt="slide-img" />
+                  </div>
+                  <div className="slide">
+                    <img src={aboutImg} alt="slide-img" />
+                  </div>
+                </Slider>
               </div>
             </div>
           </div>
