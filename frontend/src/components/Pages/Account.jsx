8bc33594045db4a53237777adc4c475996f0d51e
Zanetti Jordan <jzanetti1@outlook.fr>
2023-07-26 15:44:11 +0200

    Add base content Account component

--- frontend/src/components/Pages/Account.jsx
+++ frontend/src/components/Pages/Account.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 const Account = () => {
   return (
-    <div className="account-page section">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <h1>My account</h1>
+    <div className="account-page">
+      <section className="section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="account-content">
+                <h1>My account</h1>
+                <p>Username : </p>
+                <p>Email :</p>
+                <p>Supprimer le compte</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
