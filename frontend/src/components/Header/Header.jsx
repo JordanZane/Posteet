24efbb3ec9e8f4a10b73a0a5b9aa3d5e81cd3c1b
Zanetti Jordan <jzanetti1@outlook.fr>
2023-09-21 09:42:30 +0200

    Add styles responsive header & new nav

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -79,6 +79,42 @@ const Header = ({ isLogged, setIsLogged }) => {
                   )}
                 </ul>
               </nav>
+              <nav className="sm-nav">
+                <ul>
+                  <li>
+                    <NavLink to="/">
+                      <i class="fa-solid fa-house"></i>
+                    </NavLink>
+                  </li>
+                  {isLogged ? (
+                    <>
+                      <li>
+                        <NavLink to={`/dashboard/${userId}`}>Dashboard</NavLink>
+                      </li>
+                      <li>
+                        <button onClick={HandleSubNav}>
+                          <i className="fa-solid fa-user"></i>
+                        </button>
+                        <ul id="subnav">
+                          <li>
+                            <Link to={`/my-account/${userId}`}>My account</Link>
+                          </li>
+                          <li>Dark Mode</li>
+                          <li>
+                            <button onClick={handleLogout}>Déconnexion</button>
+                          </li>
+                        </ul>
+                      </li>
+                    </>
+                  ) : (
+                    <li className="link-container">
+                      <NavLink to="/sign-up">Sign-up</NavLink>
+                      {' / '}
+                      <NavLink to="/log-in">Log-in</NavLink>
+                    </li>
+                  )}
+                </ul>
+              </nav>
             </div>
           </div>
         </div>
